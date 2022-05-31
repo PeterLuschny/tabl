24da2a32ff893752b61f9c4b12c672c3a462b25f
PeterLuschny <peter.luschny@gmail.com>
2022-05-31 22:00:55 +0200

    schroeder

---
+++ src/CatalanStreched.py
@@ -0,0 +1,38 @@
+from functools import cache
+from tablgenerator import TablGenerator
+
+"""The streched Catalan triangle, A053121. 
+
+[0]   1,
+[1]   0,   1,
+[2]   1,   0,   1,
+[3]   0,   2,   0,   1,
+[4]   2,   0,   3,   0,   1,
+[5]   0,   5,   0,   4,   0,   1,
+[6]   5,   0,   9,   0,   5,   0,   1,
+[7]   0,  14,   0,  14,   0,   6,   0,  1,
+[8]  14,   0,  28,   0,  20,   0,   7,  0,  1,
+[9]   0,  42,   0,  48,   0,  27,   0,  8,  0,  1.
+"""
+
+
+@cache
+def _sca(n: int) -> list[int]:
+    if n == 0:
+        return [1]
+    r = lambda k: _sca(n - 1)[k] if k >= 0 and k < n else 0
+    row = _sca(n - 1) + [1]
+    for k in range(0, n):
+        row[k] = r(k - 1) + r(k + 1)
+    return row
+
+
+catalan_streched = TablGenerator(_sca)
+
+
+####################################################################
+
+if __name__ == "__main__":
+    from tabltest import TablTest
+
+    TablTest(catalan_streched)
---
+++ src/Schroeder.py
@@ -0,0 +1,39 @@
+from functools import cache
+from tablgenerator import TablGenerator
+
+""" Schroeder triangle, A122538
+
+[0] [1]
+[1] [0,     1]
+[2] [0,     2,     1]
+[3] [0,     6,     4,     1]
+[4] [0,    22,    16,     6,    1]
+[5] [0,    90,    68,    30,    8,    1]
+[6] [0,   394,   304,   146,   48,   10,   1]
+[7] [0,  1806,  1412,   714,  264,   70,  12,   1]
+[8] [0,  8558,  6752,  3534, 1408,  430,  96,  14,  1]
+[9] [0, 41586, 33028, 17718, 7432, 2490, 652, 126, 16, 1]
+"""
+
+
+@cache
+def _sch(n: int) -> list[int]:
+    if n == 0:
+        return [1]
+    if n == 1:
+        return [0, 1]
+    row = _sch(n - 1) + [1]
+    for k in range(n - 1, 0, -1):
+        row[k] += row[k - 1] + row[k + 1]
+    return row
+
+
+schroeder = TablGenerator(_sch)
+
+
+####################################################################
+
+if __name__ == "__main__":
+    from tabltest import TablTest
+
+    TablTest(schroeder)
