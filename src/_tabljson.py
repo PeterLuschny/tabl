c16326ed802e2834844411975a1a860dd493174a
PeterLuschny <peter.luschny@gmail.com>
2024-09-27 18:00:25 +0200

    llk

--- src/_tabljson.py
+++ src/_tabljson.py
@@ -152,25 +152,28 @@ def search(query: str) -> str:
         str: The sequence number in the OEIS format (e.g., A000001).
 
     """
+    print("\nSearched  :", query)
     jdata = get(f"https://oeis.org/search?q={query}&fmt=json", timeout=10).json()
     count = jdata["count"]
-    print(count)
-    anumber = ""
-    if count > 0:
-        seq = jdata["results"][0]
-        number = seq["number"]
-        data = seq["data"]
-        print(number)
-        print(data)
-        # [int(term) for term in data.split(",")]
-        anumber = f"A{(6 - len(str(number))) * '0' + str(number)}"
-        print(anumber)
-    else:
+    if count == 0:
         print("Sorry, nothing found!")
+        return ""
+    
+    print("Seqs found:", count)
+    seq = jdata["results"][0]
+
+    number = seq["number"]
+    anumber = f"A{(6 - len(str(number))) * '0' + str(number)}"
+    print("A-number  :", anumber)
+
+    data = seq["data"]
+    print("Sequence  :", data)
+    # [int(term) for term in data.split(",")]
     return anumber
 
 
 if __name__ == "__main__":
+
     seqs = search("1, 2, 3, 4, 5")
     seqs = search("1, 4, 1, 9, 9, 2, 16, 36,")
     seqs = search("9991, 2, 48323, 4, 0")
